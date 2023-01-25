9721ca023aea28237ad5cf89d4e8ba99004a9a84
후디 이야기Hoodie’s Story <shinm93@gmail.com>
2023-01-26 00:37:23 +0900

    로그인을 요청하는 fetch 생성

--- app/app.js
+++ app/app.js
@@ -8,7 +8,7 @@ const app = express();
 const home = require("./src/routers/home"); // 이미 만든 js폴더를 불러오기 위함. 폴더 위치를 상대적으로 명시해주어야함
 
 // 앱 세팅
-app.set("views", "./src/views");
+app.set("views", "./src/views");                                                                                                                                                                                                                                                     
 app.set("view engine", "ejs");
 
 app.use("/", home); // use ->  미들웨어를 등록해주는 메서드
--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -11,5 +11,38 @@ function login() {
     id: id.value,
     psword: psword.value,
   };
-  console.log(req);
+
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req),
+  });
 }
+
+/* Restful : API
+ 서버와 클라이언트간의 약속 중 하나가 restful
+
+
+ <Rest : 사이트 구성원리>
+ (정의) rest란?
+  : 분산 시스템 설계를 위한 아케텍쳐(구성) 스타일
+  : 네트워크 아키텍쳐 원리모음
+  : 제약 조건의 집합
+  : > 간단하게 [사이트 구성 원리]라고 이해하면 됨
+
+  REpresentational (표현)
+  State            (상태)
+  Transter         (전송)
+  =>자원의 표현을 가지고 상태를 전달한다.
+  1.자원 : URL 
+  2.표현 : Header (text/html, image/gif, test/* 등)
+  3.상태 : Method (GET, POST, PUT, DELETE)
+
+  <Restful : Rest(사이트 구성을 따르는)한 >
+  <Restful API : Rest한 API>
+
+
+
+*/
