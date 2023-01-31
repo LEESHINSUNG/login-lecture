0a0fb7cb3a601a7671d622a30eb69c04fb7127fd
후디 이야기Hoodie’s Story <shinm93@gmail.com>
2023-02-01 00:17:01 +0900

    백엔드에서 로그인 기능 구현

--- app/app.js
+++ app/app.js
@@ -2,16 +2,21 @@
 
 // 모듈
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 // 라우팅
 const home = require("./src/routers/home"); // 이미 만든 js폴더를 불러오기 위함. 폴더 위치를 상대적으로 명시해주어야함
 
 // 앱 세팅
-app.set("views", "./src/views");                                                                                                                                                                                                                                                     
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
+app.use(express.static(`${__dirname}/src/public`));
+app.use(bodyParser.json());
+// URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use("/", home); // use ->  미들웨어를 등록해주는 메서드
-app.use(express.static(`${__dirname}/src/public`))
 
 module.exports = app;
