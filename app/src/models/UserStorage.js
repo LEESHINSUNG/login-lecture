9047ccc3875978089f9f7a6f4ae375523a7b81e0
후디 이야기Hoodie’s Story <shinm93@gmail.com>
2023-03-01 00:45:11 +0900

    User 모델 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -13,9 +13,19 @@ class UserStorage {
       if (users.hasOwnProperty(field)) {
         newUsers[field] = users[field];
       }
-      return newUsers
+      return newUsers;
     }, {});
     return newUsers;
+  }
+  static getUserInfo(id) {
+    const users = this.#users;
+    const idx = users.id.indexOf(id)
+    const userInfo = Object.keys(users).reduce((newUser, info) => {
+      newUser[info] = users[info][idx];
+      return newUser;
+    }, {});
+
+    return userInfo;
   }
 }
 
--- app/src/routers/home/home.ctrl.js
+++ app/src/routers/home/home.ctrl.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const User = require("../../models/User");
 const UserStorage = require("../../models/UserStorage");
 
 const output = {
@@ -9,34 +10,15 @@ const output = {
   login: (req, res) => {
     res.render("home/login");
   },
-  register: (req,res) => {
-    res.render("home/register")
-  }
+  register: (req, res) => {
+    res.render("home/register");
+  },
 };
 
-// const users = {
-//   id: ["hoodie", "신개발", "이팀장"],
-//   psword: ["1234", "1234", "123456"],
-// };
-  
 const process = {
   login: (req, res) => {
-    const id = req.body.id;
-    const psword = req.body.psword;
-
-    // class를 직접사용 하고 변수에 접근할 때 그냥 사용하면 undefined가 뜸, 그래서 class가 있는 파일에서 static선언을 해줘야 변수에 접근 가능함
-    const users = UserStorage.getUsers("id", "psword");
-    const response = {};
-    if (users.id.includes(id)) {
-      const idx = users.id.indexOf(id);
-      if (users.psword[idx] === psword) {
-        response.success = true;
-        return res.json(response);
-      }
-    }
-
-    response.success = false;
-    response.msg = "로그인에 실패하셨습니다.";
+    const user = new User(req.body);
+    const response = user.login();
     return res.json(response);
   },
 };
@@ -45,3 +27,25 @@ module.exports = {
   output,
   process,
 };
+
+// const process = {
+//   login: (req, res) => {
+//     const id = req.body.id;
+//     const psword = req.body.psword;
+
+//     // class를 직접사용 하고 변수에 접근할 때 그냥 사용하면 undefined가 뜸, 그래서 class가 있는 파일에서 static선언을 해줘야 변수에 접근 가능함
+//     const users = UserStorage.getUsers("id", "psword");
+//     const response = {};
+//     if (users.id.includes(id)) {
+//       const idx = users.id.indexOf(id);
+//       if (users.psword[idx] === psword) {
+//         response.success = true;
+//         return res.json(response);
+//       }
+//     }
+
+//     response.success = false;
+//     response.msg = "로그인에 실패하셨습니다.";
+//     return res.json(response);
+//   },
+// };
