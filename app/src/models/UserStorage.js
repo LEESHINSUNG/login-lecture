a3e9bc1732ec9ff4a5c8ab39c954a533359f5ddb
후디 이야기Hoodie’s Story <shinm93@gmail.com>
2023-03-08 23:36:03 +0900

    fs모듈을 통한 로근인 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,24 +1,10 @@
 "use strict";
 
-class UserStorage {
-  static #users = {
-    id: ["hoodie", "신개발", "이팀장"],
-    psword: ["1234", "1234", "123456"],
-    name: ["후디", "신개발", "이팀장"],
-  };
+const fs = require("fs").promises;
 
-  static getUsers(...fields) {
-    const users = this.#users;
-    const newUsers = fields.reduce((newUsers, field) => {
-      if (users.hasOwnProperty(field)) {
-        newUsers[field] = users[field];
-      }
-      return newUsers;
-    }, {});
-    return newUsers;
-  }
-  static getUserInfo(id) {
-    const users = this.#users;
+class UserStorage {
+  static #getUserInfo(data, id) {
+    const users = JSON.parse(data);
     const idx = users.id.indexOf(id);
     const userInfo = Object.keys(users).reduce((newUser, info) => {
       newUser[info] = users[info][idx];
@@ -28,8 +14,28 @@ class UserStorage {
     return userInfo;
   }
 
+  static getUsers(...fields) {
+    // const users = this.#users;
+    const newUsers = fields.reduce((newUsers, field) => {
+      if (users.hasOwnProperty(field)) {
+        newUsers[field] = users[field];
+      }
+      return newUsers;
+    }, {});
+    return newUsers;
+  }
+
+  static getUserInfo(id) {
+    return fs
+      .readFile("./src/databases/users.json")
+      .then((data) => {
+        return this.#getUserInfo(data, id);
+      })
+      .catch(console.log);
+  }
+
   static save(userInfo) {
-    const users = this.#users;
+    // const users = this.#users;
     users.id.push(userInfo.id);
     users.name.push(userInfo.name);
     users.psword.push(userInfo.psword);
